4af8f25e002c20d7dc21d30b0a637235f21d3232
Alexander Govyashov <ag@talenttech.ru>
2021-04-19 12:17:48 +0300

    initial commit

---
+++ bin/pr_reviewers_ping.dart
@@ -0,0 +1,5 @@
+import 'package:pr_reviewers_ping/pr_reviewers_ping.dart' as pr_reviewers_ping;
+
+void main(List<String> arguments) {
+  pr_reviewers_ping.pingPullRequestReviewers();
+}
---
+++ test/pr_reviewers_ping_test.dart
@@ -0,0 +1,5 @@
+import 'package:pr_reviewers_ping/pr_reviewers_ping.dart';
+
+void main() {
+
+}
